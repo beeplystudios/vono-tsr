f21d1acb4800e3b4e0def05e4ba2f5e6398e1bd0
Gilbert Zhang <gilbertzhang1208@gmail.com>
2024-01-09 06:46:19 -0500

    fix tanstack router streaming

--- server/renderer.tsx
+++ server/renderer.tsx
@@ -1,5 +1,8 @@
 import { createMemoryHistory } from "@tanstack/react-router";
-import { StartServer } from "@tanstack/react-router-server/server";
+import {
+  StartServer,
+  transformStreamWithRouter,
+} from "@tanstack/react-router-server/server";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import React from "react";
 import { createHeadCore } from "unhead";
@@ -7,6 +10,8 @@ import { createRouter } from "../src/router";
 import { AppRouter } from "./trpc";
 // @ts-expect-error idk
 import manifest from "#vono/manifest";
+import { Readable, Transform } from "stream";
+import { ReadableStream as NodeReadableStream } from "node:stream/web";
 
 const dev = true;
 
@@ -15,7 +20,8 @@ import RefreshRuntime from 'http://localhost:5173/@react-refresh'
 RefreshRuntime.injectIntoGlobalHook(window)
 window.$RefreshReg$ = () => {}
 window.$RefreshSig$ = () => (type) => type
-window.__vite_plugin_react_preamble_installed__ = true`;
+window.__vite_plugin_react_preamble_installed__ = true
+`;
 
 const Shell = (props: {
   head: React.ReactNode;
@@ -79,7 +85,7 @@ export default async function render(url: string) {
     })
   );
 
-  return renderToReadableStream(
+  const stream: NodeReadableStream = await renderToReadableStream(
     <Shell
       scripts={
         <script
@@ -98,4 +104,14 @@ export default async function render(url: string) {
       <StartServer router={router} />
     </Shell>
   );
+
+  // convert the web ReadableStream to Nodejs Readable, pipe it throw the
+  // tanstack router transform and convert it back to a web ReadableStream
+  // if we can convert between web and node streams,
+
+  // we should prob use the pipeable streams api for react dom server to avoid
+  // having to copy a load of data every request (does it work like that?)
+  const transform = transformStreamWithRouter(router);
+  const streamWithRouterData = Readable.fromWeb(stream).pipe(transform);
+  return Transform.toWeb(streamWithRouterData).readable;
 }
