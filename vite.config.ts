610ce6baf51e2a18a50491d59739a03eb086cbc8
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-01-10 15:56:44 -0500

    switch package to @vonojs/vono

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vono from "@gaiiaa/vono";
+import vono from "@vonojs/vono";
 
 // https://vitejs.dev/config/
 export default defineConfig({
